94ea10887cbbfacdd3f55dc5c374ecec8f1de04b
Aurora Sloan <lbaldinell@gmail.com>
2021-03-30 01:25:29 +0900

    update program properties

--- python_bootcamp/module00/ex05/kata01.py
+++ python_bootcamp/module00/ex05/kata01.py
@@ -2,6 +2,9 @@ languages = {
     'Python': 'Guido van Rossum',
     'Ruby': 'Yukihiro Matsumoto',
     'PHP': 'Rasmus Lerdorf',
-    }
-for lang in languages:
-    print(lang, "was created by ", languages[lang])
+}
+
+
+if __name__ == "__main__":
+    for lang in languages:
+        print(lang, "was created by", languages[lang])
