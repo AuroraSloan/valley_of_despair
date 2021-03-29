9aa209f5fe0bf2de6aaba264315541bd25957eb0
Aurora Sloan <lbaldinell@gmail.com>
2021-03-30 00:46:06 +0900

    add program properties

--- python_bootcamp/module00/ex02/whois.py
+++ python_bootcamp/module00/ex02/whois.py
@@ -1,15 +1,20 @@
 import sys
 
-if (len(sys.argv) == 2):
-    arg = sys.argv[1]
-    if arg.isdigit():
-        if(int(arg) == 0):
-            print("I'm Zero.")
-        elif(int(arg) % 2 == 0):
-            print("I'm Even.")
+
+def main():
+    if (len(sys.argv) == 2):
+        arg = sys.argv[1]
+        if arg.isdigit():
+            if(int(arg) == 0):
+                print("I'm Zero.")
+            elif(int(arg) % 2 == 0):
+                print("I'm Even.")
+            else:
+                print("I'm Odd.")
         else:
-            print("I'm Odd.")
-    else:
-        print("Error")
-elif(len(sys.argv) > 2):
-    print("ERROR")
+            print("ERROR")
+    elif(len(sys.argv) > 2):
+        print("ERROR")
+
+if __name__ == "__main__":
+    main()
