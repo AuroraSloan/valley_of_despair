e6250bb4fd2ce786cf1e532fbdba14830802a310
Aurora Sloan <lbaldinell@gmail.com>
2021-03-30 00:24:58 +0900

    improve exec.py

--- python_bootcamp/module00/ex01/exec.py
+++ python_bootcamp/module00/ex01/exec.py
@@ -1,11 +1,10 @@
 import sys
-argc = len(sys.argv)
-if(argc > 1):
-    string = sys.argv[1]
-    i = 2
-    while(i < argc):
-        string += ' '
-        string += sys.argv[i]
-        i += 1
-    reverse = string[::-1]
-    print(reverse.swapcase())
+
+
+def main():
+    s = ' '.join(sys.argv[1::])
+    print(s.swapcase()[::-1])
+
+
+if __name__ == "__main__":
+    main()
