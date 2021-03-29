e948827bbb0491146dc15c2d0275fcd4f16a6eaf
Aurora Sloan <lbaldinell@gmail.com>
2021-03-30 01:18:39 +0900

    improved

--- python_bootcamp/module00/ex05/kata00.py
+++ python_bootcamp/module00/ex05/kata00.py
@@ -1,7 +1,19 @@
 t = (19,42,21,234,6,2.2,1234,2,1.31, 1,2,2,2)
-print(f"The {len(t)} numbers are: ", end='')
-for i, num in enumerate(t):
-    if (i == len(t) - 1):
-        print(f"{num}")
-    else:
-        print(f"{num}, ", end='')
+
+
+def main():
+    print(f"The {len(t)} numbers are: " + (str(t))[1:-1:])
+
+
+if __name__ == "__main__":
+    main()
+
+
+# For + enumerate practice/reminder
+# def main():
+#    print(f"The {len(t)} numbers are: ", end='')
+#    for i, num in enumerate(t):
+#        if (i == len(t) - 1):
+#            print(f"{num}")
+#        else:
+#            print(f"{num}, ", end='')
